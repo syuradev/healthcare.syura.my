09df9066b00f27fd2b199ecd2fca8a4cb2019814
zapirolabs <zapirolabs@gmail.com>
2025-08-07 10:31:41 +0800

    change login layout

---
+++ src/app/auth/login/layout.js
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: "Login - MedSyura Portal",
+  description: "Login to your MedSyura account",
+};
+
+export default function LoginLayout({ children }) {
+  return children;
+} 
